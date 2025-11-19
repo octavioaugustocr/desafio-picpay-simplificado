cde9ac99ccb2a55fb62f86a232903cb79445f568
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-18 22:22:27 -0300

    colocando as validações/serviços do TransferRepository no TransferService

--- Repositories/Transfer/TransferRepository.cs
+++ Repositories/Transfer/TransferRepository.cs
@@ -14,33 +14,6 @@ public class TransferRepository : ITransferRepository
     {
         _appDbContext = appDbContext;
     }
-    
-    private async Task<bool> ExternalAuthorizationService()
-    {
-        var client = new HttpClient();
-
-        var response = await client.GetAsync("https://util.devi.tools/api/v2/authorize");
-        if (!response.IsSuccessStatusCode) return false;
-        
-        return true;
-    }
-
-    private async Task<bool> ValidationsTransfer(MakeTransferDto makeTransferDto)
-    {
-        if (makeTransferDto.Payer == makeTransferDto.Payee) return false;
-        
-        var payer = _appDbContext.User.Find(makeTransferDto.Payer);
-        var payee = _appDbContext.User.Find(makeTransferDto.Payee);
-        if (payer == null || payee == null) return false;
-        
-        if (payer.TypeUser == TypeUserEnum.Merchant) return false;
-        if (payer.Balance < makeTransferDto.Value) return false;
-        
-        var responseExternalAuthorization = await ExternalAuthorizationService();
-        if (!responseExternalAuthorization) return false;
-
-        return true;
-    }
 
     private async Task<bool> FinalizeTransfer(MakeTransferDto makeTransferDto)
     {
@@ -63,17 +36,6 @@ public class TransferRepository : ITransferRepository
         }
     }
 
-    private async Task<bool> SendTransferNotification()
-    {
-        var client = new HttpClient();
-        
-        var response = await client.GetAsync("https://util.devi.tools/api/v1/notify");
-        if (!response.IsSuccessStatusCode) return false;
-        
-        // Enviar notificação..
-        return true;
-    }
-
     public async Task<TransferModel> GetTransferById(int id)
     {
         return await _appDbContext.Transfer.FindAsync(id);
@@ -86,9 +48,6 @@ public class TransferRepository : ITransferRepository
 
     public async Task<TransferModel> MakeTransfer(MakeTransferDto makeTransferDto)
     {
-        var response = await ValidationsTransfer(makeTransferDto);
-        if (!response) return null;
-
         var transferModel = new TransferModel()
         {
             Value = makeTransferDto.Value,
@@ -101,8 +60,6 @@ public class TransferRepository : ITransferRepository
         if (!await FinalizeTransfer(makeTransferDto)) return null;
         
         await _appDbContext.SaveChangesAsync();
-        
-        await SendTransferNotification();
         
         return transferModel;
     }
--- Services/Transfer/TransferService.cs
+++ Services/Transfer/TransferService.cs
@@ -1,16 +1,41 @@
 ﻿using desafio_picpay_simplificado.Dtos;
+using desafio_picpay_simplificado.Enums;
 using desafio_picpay_simplificado.Models;
 using desafio_picpay_simplificado.Repositories.Transfer;
+using desafio_picpay_simplificado.Services.User;
 
 namespace desafio_picpay_simplificado.Services.Transfer;
 
 public class TransferService : ITransferService
 {
     private readonly ITransferRepository _transferRepository;
+    private readonly IUserService _userService;
 
-    public TransferService(ITransferRepository transferRepository)
+    public TransferService(ITransferRepository transferRepository, IUserService userService)
     {
         _transferRepository = transferRepository;
+        _userService = userService;
+    }
+    
+    private async Task<bool> ExternalAuthorizationService()
+    {
+        var client = new HttpClient();
+
+        var response = await client.GetAsync("https://util.devi.tools/api/v2/authorize");
+        if (!response.IsSuccessStatusCode) return false;
+        
+        return true;
+    }
+    
+    private async Task<bool> SendTransferNotification()
+    {
+        var client = new HttpClient();
+        
+        var response = await client.GetAsync("https://util.devi.tools/api/v1/notify");
+        if (!response.IsSuccessStatusCode) return false;
+        
+        // Enviar notificação..
+        return true;
     }
 
     public async Task<TransferModel> GetTransferById(int id)
@@ -22,9 +47,33 @@ public class TransferService : ITransferService
     {
         return await _transferRepository.GetAllTransfers();
     }
+    
+    private async Task<bool> ValidationsTransfer(MakeTransferDto makeTransferDto)
+    {
+        if (makeTransferDto.Payer == makeTransferDto.Payee) return false;
+        
+        var payer = await _userService.GetUserById(makeTransferDto.Payer);
+        var payee = await _userService.GetUserById(makeTransferDto.Payee);
+        if (payer == null || payee == null) return false;
+        
+        if (payer.TypeUser == TypeUserEnum.Merchant) return false;
+        if (payer.Balance < makeTransferDto.Value) return false;
+        
+        var responseExternalAuthorization = await ExternalAuthorizationService();
+        if (!responseExternalAuthorization) return false;
+
+        return true;
+    }
 
     public async Task<TransferModel> MakeTransfer(MakeTransferDto makeTransferDto)
     {
-        return await _transferRepository.MakeTransfer(makeTransferDto);
+        var response = await ValidationsTransfer(makeTransferDto);
+        if (!response) return null;
+        
+        var makeTransferResponse = await _transferRepository.MakeTransfer(makeTransferDto);
+        if (makeTransferResponse == null) return null;
+
+        await SendTransferNotification();
+        return makeTransferResponse;
     }
 }
